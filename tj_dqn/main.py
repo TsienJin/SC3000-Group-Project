cc63ec0b5d6a26af0741adb2ed7a6b5457ca18b7
Tsien Jin <tsien.jin@gmail.com>
2023-02-17 14:41:16 +0800

    pooppyy

--- tj_dqn/main.py
+++ tj_dqn/main.py
@@ -7,13 +7,17 @@ from localTypes import *
 
 class Agent:
     MEM_SIZE = 50_000
+    MIN_MEM_SIZE = 10_000
     MIN_MEMORY_LEN = 1_000
     DISCOUNT = 0.99
     LEARNING_RATE = 0.0001
     GAMMA = 0.95
-    EXP_DECAY = 0.999
-    EXP_MIN = 0.001
-    EXP_MAX = 1.0
+    EPS = 0.5
+    EPS_DECAY = 0.999
+    EPS_MIN = 0.001
+    EPS_MAX = 1.0
+    MIN_BATCH = 64
+    TARGET_UPDATE_FREQ = 5
 
     def __init__(self):
 
@@ -21,7 +25,10 @@ class Agent:
         self.model = DQN(n_obsv=4, n_actions=2, memory=self.memory)
         self.targetModel = deepcopy(self.model)
 
-    def
+        self.targetUpdateCounter = 0
+
+    def addToMem(self, record:Record):
+        self.memory.push(record)
 
 
 
